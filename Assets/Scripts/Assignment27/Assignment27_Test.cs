c0a88d349122289201f5ebc90fc118e1b28aa675
rolanredwan <rolanredwan@gmail.com>
2024-12-28 14:28:58 +0200

    importan changes.

--- Assets/Scripts/Assignment27/Assignment27_Test.cs
+++ Assets/Scripts/Assignment27/Assignment27_Test.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Assignment27
 {
-    public class Assignment27_Teat : MonoBehaviour
+    public class Assignment27_Test : MonoBehaviour
     {
         void Start()
         {
-            int a = 0;
-            int b = 1;
+            int a = 10;
+            int b = 20;
+
+            IncrementNum1(a);
+            Debug.Log(a);
+
+            // ------------------------------------
+
+            IncrementNum2(ref b);
+            Debug.Log(b);
+
+            // ------------------------------------
+
+            int d;
+            TestOutKeyword(out d);
+            Debug.Log(d);
         }
 
-        public void modifyNumcer(int num){
-            
+        public void IncrementNum1(int num){
+            num+= 10 ;
+        }
+
+        public void IncrementNum2(ref int num){
+            num += 10;
+        }
+
+        public void TestOutKeyword(out int num){
+            num = 20;
         }
     }
 }
